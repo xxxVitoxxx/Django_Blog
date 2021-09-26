468de74211efbf8320c3b1bbb586dc7bbb02124e
宏育趙 <bird06291994@gmail.com>
2021-09-27 00:53:33 +0800

    login

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register
+from .views import register, login
 
 app_name = 'account'
 urlpatterns = [
-    path('register/', register, name='register')
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+
 from .forms import UserForm
 # Create your views here.
 
@@ -18,4 +21,29 @@ def register(request):
     
     userForm.save()
     messages.success(request, '歡迎註冊')
+    return redirect('blog_app:main')
+
+def login(request):
+    '''
+    login as existing user
+    '''
+    template = 'account/login.html'
+    if request.method == 'GET':
+        return render(request, template)
+
+    # POST
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not username or not password: # server-side validation
+        messages.error(request, '請填資料')
+        return render(request, template)
+    
+    user = authenticate(username=username, password=password)
+    if not user: # authentication fails
+        messages.error(request, '登入失敗')
+        return render(request, template)
+
+    # login succcess
+    auth_login(request, user)
+    messages.success(request, '登入成功')
     return redirect('blog_app:main')
